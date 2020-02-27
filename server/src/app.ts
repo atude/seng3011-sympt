b2778e24f14de89e11e0130f2d5ca608535af086
Mozamel Anwary <mozamel.anwary1@gmail.com>
2020-02-27 23:45:57 +1100

    pupeteer screenshot promed page after search example

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,7 +15,7 @@ const port = 4000;
 // Example request from firestore
 const getUsername = async (userId: string) => {
 
-  // --> Usually you would also text for authentication
+  // --> Usually you would also test for authentication
   // here before calling the function, i.e. the function 
   // should receive some sort of authentication token here which 
   // your firebase admin account can verify that the client
@@ -40,6 +40,25 @@ const getUsername = async (userId: string) => {
   }
 }
 
+const puppeteer = require('puppeteer');
+
+const startPuppeteer = async () => {
+  const browser = await puppeteer.launch({
+    defaultViewport: {width: 1920, height: 1080}
+  });
+  const page = await browser.newPage();
+  await page.goto('https://promedmail.org/promed-posts/');
+  await page.$eval('#searchterm', (el: { value: string; }) => el.value = "coronavirus");
+  const submitButton = await page.$('form[name="as_form"] input[type="submit"]');
+  await submitButton.click();
+  // await page.click('input[type="submit"]');
+  await page.waitFor(10000);
+  await page.screenshot({path: 'test.png'});
+  console.log("Screenshotted");
+  // other actions...
+  await browser.close();
+};
+
 /* 
 
 --> Heres an example of filtering through a firestore collection
@@ -60,6 +79,7 @@ const usersDocs = usersCollection.docs.map((doc) => doc.data());
 
 app.get('/', async (req, res) => {
   const getData = await getUsername("test");
+  await startPuppeteer();
   res.send(getData);
 });
 
