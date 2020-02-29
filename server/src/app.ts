94d96c1fa9c16cfbdb4aad329f19e44e2d5eaeaf
Matthew Freeman <mattyfreeman101@gmail.com>
2020-02-29 15:38:17 +1100

    commented out call to firebase as it was causing server crashes. imported new puppeteer function and called function in the main section

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,23 +1,26 @@
 /* eslint-disable no-console */
-import * as express from 'express';
-import * as admin from 'firebase-admin';
+import * as express from "express";
+import * as admin from "firebase-admin";
+import promedURLResults from "./puppeteer";
 
-const serviceAccount = require('../service-account.json');
+const serviceAccount = require("../service-account.json");
 
 // Init firebase service account
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount)
 });
 
 const app = express();
 const port = 4000;
 
-// Example request from firestore
-const getUsername = async (userId: string) => {
+/*
 
+// Example request from firestore
+
+const getUsername = async (userId: string) => {
   // --> Usually you would also test for authentication
-  // here before calling the function, i.e. the function 
-  // should receive some sort of authentication token here which 
+  // here before calling the function, i.e. the function
+  // should receive some sort of authentication token here which
   // your firebase admin account can verify that the client
   // is authenticated
   // <--
@@ -32,32 +35,50 @@ const getUsername = async (userId: string) => {
       .doc(`users/${userId}`)
       .get();
 
-      // Convert the document into usable data
-      const testUserUsername = getTestUserDocument.data();
-      return testUserUsername?.username;
-  } catch(error) {
+    // Convert the document into usable data
+    const testUserUsername = getTestUserDocument.data();
+    return testUserUsername?.username;
+  } catch (error) {
     throw new Error(error);
   }
-}
+};
 
-const puppeteer = require('puppeteer');
+*/
 
+/*
+
+const puppeteer = require("puppeteer");
+
+
+--> Heres an example of some basic puppeteer interactions
+    We start by opening a headless instance of chrome (without the GUI)
+    and wait for the page to load. We then navigate to the promed search page.
+    We find then find the input html element with id=searchterm and input our search query.
+    Create a DOM variable containing the submit button and click it.
+    Hit the submit button.
+    We wait for the page to load and then take a screen shot.
 const startPuppeteer = async () => {
   const browser = await puppeteer.launch({
-    defaultViewport: {width: 1920, height: 1080}
+    defaultViewport: { width: 1920, height: 1080 }
   });
   const page = await browser.newPage();
-  await page.goto('https://promedmail.org/promed-posts/');
-  await page.$eval('#searchterm', (el: { value: string; }) => el.value = "coronavirus");
-  const submitButton = await page.$('form[name="as_form"] input[type="submit"]');
+  await page.goto("https://promedmail.org/promed-posts/");
+  await page.$eval(
+    "#searchterm",
+    (el: { value: string }) => (el.value = "coronavirus")
+  );
+  const submitButton = await page.$(
+    'form[name="as_form"] input[type="submit"]'
+  );
   await submitButton.click();
   // await page.click('input[type="submit"]');
   await page.waitFor(10000);
-  await page.screenshot({path: 'test.png'});
+  await page.screenshot({ path: "test.png" });
   console.log("Screenshotted");
   // other actions...
   await browser.close();
 };
+*/
 
 /* 
 
@@ -76,11 +97,19 @@ const usersDocs = usersCollection.docs.map((doc) => doc.data());
 
 */
 
+app.get("/", async (req, res) => {
+  // I commented out the below call as it was
+  // causing the server to crash? - matthew
+  //const getData = await getUsername("test");
 
-app.get('/', async (req, res) => {
-  const getData = await getUsername("test");
-  await startPuppeteer();
-  res.send(getData);
+  //await startPuppeteer();
+  //add await to the function call below
+  await promedURLResults(
+    "?keyterms=coronavirus&startdate=2019-12-01T00:00:00&enddate=2020-02-01T00:00:00&location=china"
+  );
+  res.send("Hi!");
 });
 
-app.listen(process.env.PORT || port, () => console.log(`Server is listening on ${port}`));
+app.listen(process.env.PORT || port, () =>
+  console.log(`Server is listening on ${port}`)
+);
