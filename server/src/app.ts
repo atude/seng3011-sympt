187c8c271923b324e14f3bbf24ed81a2c94d60e0
Mozamel Anwary <mozamel.anwary1@gmail.com>
2020-03-14 18:04:12 +1100

    change port config

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,7 +16,7 @@ admin.initializeApp({
 import queryScrapePosts from './queryController';
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 const exampleQuery = '?keyterms=coronavirus&startdate=2019-12-01T00:00:00&enddate=2020-02-01T00:00:00&location=china';
 
@@ -24,4 +24,4 @@ app.get('/', async (req, res) => {
   res.send(await queryScrapePosts(exampleQuery));
 });
 
-app.listen(process.env.PORT || port, () => console.log(`--> Server is listening on ${port}`));
+app.listen(port, () => console.log(`--> Server is listening on ${port}`));
