51830636f6d9901cb3271d69282ffae9dee0d9bc
Mozamel Anwary <mozamel.anwary1@gmail.com>
2020-03-14 18:35:43 +1100

    experiment routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -20,7 +20,7 @@ const port: number = Number(process.env.PORT) || 4000;
 
 const exampleQuery = '?keyterms=coronavirus&startdate=2019-12-01T00:00:00&enddate=2020-02-01T00:00:00&location=china';
 
-app.get('/', async (req, res) => {
+app.get('/api', async (req, res) => {
   res.send(await queryScrapePosts(exampleQuery));
 });
 
