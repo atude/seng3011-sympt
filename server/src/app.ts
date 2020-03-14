fee46702b9dc8b65686aab581d174bf3bb03d685
Matthew Freeman <mattyfreeman101@gmail.com>
2020-03-14 14:56:00 +1100

    modified the express app.get body to set an interval which iterates over the list of diseases and prints the results to the console for now. needs to be extended to connect with the firestore db. results are only the top 20 or so results.

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,9 @@
 /* eslint-disable import/first */
 import express from 'express';
 import * as admin from 'firebase-admin';
+// import queryScrapePosts from './queryController';
+import diseaseList from './constants/diseaseList.json';
+import firestoreScrapedPosts from './firestoreQueryController';
 
 require('dotenv').config();
 const serviceAccount = require('../service-account.json');
@@ -18,11 +21,32 @@ import queryScrapePosts from './queryController';
 const app = express();
 const port: number = Number(process.env.PORT) || 4000;
 
-// const exampleQuery = '?keyterms=coronavirus&startdate=2019-12-01T00:00:00&enddate=2020-02
-// -01T00:00:00&location=china';
+function scrapePromed(i: number) {
+  setTimeout(async () => {
+    const searchDisease: string = diseaseList[i].name.split(' ').join(' AND ');
+    console.log(`searching on terms: ${searchDisease}`);
+    // the query url is start/end date and location agnostic only taking the most recent results
+    const queryURL: string = `?keyterms=${searchDisease}`;
+    const results: Promise<any> = await firestoreScrapedPosts(queryURL);
+    console.log(results); // debugging print statement
+    // ****************************************************************
+    // insert firestore connection and population here based on results
+    // ****************************************************************
+  }, 120 * 1000 * i);
+}
+
+// const exampleQuery = '?keyterms=coronavirus&startdate=2019-12-01T00:00:00&
+// enddate=2020-02-01T00:00:00&location=china';
 
 app.get('/', async (req, res) => {
-  res.send(await queryScrapePosts(exampleQuery));
+  setInterval(() => {
+    for (let i: number = 0; i < diseaseList.length; i++) {
+      scrapePromed(i);
+    }
+  }, 60 * 60 * 12 * 1000); // run every 12 hours
+
+  // res.send(await queryScrapePosts(exampleQuery));
+  // res.redirect('/4000');
 });
 
 app.listen(port, '0.0.0.0', () => console.log(`--> Server is listening on ${port}`));
