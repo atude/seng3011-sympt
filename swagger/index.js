97e066729bdd4707740c39868938b19e0a41f8a3
Mariya <mar.shmalko@gmail.com>
2020-03-15 21:56:19 +1100

    Added articles-force

--- swagger/index.js
+++ swagger/index.js
@@ -7,7 +7,7 @@ var fs = require('fs'),
 var app = require('connect')();
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
-var serverPort = 8000;
+var serverPort = 8002;
 
 // swaggerRouter configuration
 var options = {
