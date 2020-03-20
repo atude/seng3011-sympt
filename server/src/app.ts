b3d92c14b3fa2fe41de37c1533bd41201e585696
Mozamel Anwary <mozamel.anwary1@gmail.com>
2020-03-20 17:56:10 +1100

    Inetgrate auth into endpoint

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,7 @@ import { getArticles } from './queryController';
 import populateDb from './services/dbPopulationService';
 import data from './utils/data'
 import { checkAuthenticated } from './services/firebaseService';
+import generateError from './utils/generateError';
 
 const app = express();
 const port: number = Number(process.env.PORT) || 4000;
@@ -17,17 +18,18 @@ console.log(`Admin init: ${!!admin}`);
 
 app.get('/articles/', async (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
-  const result = {data, articles: await getArticles(req.query)};
-  res.send(result);
+  const authenticated = await checkAuthenticated(req.headers.authorization);
+  if (authenticated) {
+    res.send(await getArticles(req.query));
+  } else {
+    res.send(generateError(401, "Bad Authentication", "Failed to verify authentication token"));
+  }
+  
 });
 
 app.get('/testauth', async (req, res) => {
-  const authenticated = await checkAuthenticated(req.headers.authorization);
-  if (authenticated) {
-    res.send("You're authenticated");
-  } else {
-    res.send("Not authenticated");
-  }
+  
+  
 });
 
 app.listen(port, '0.0.0.0', () => console.log(`--> Server is listening on ${port}`));
