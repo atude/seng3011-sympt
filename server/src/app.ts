82bb5921fb072f630736f22969036193bbd04131
Mozamel Anwary <mozamel.anwary1@gmail.com>
2020-03-20 20:15:42 +1100

    Attempt fix CORS headers

--- server/src/app.ts
+++ server/src/app.ts
@@ -19,7 +19,7 @@ console.log(`Admin init: ${!!admin}`);
 
 app.all('/*', (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization");
   next();
 });
 
