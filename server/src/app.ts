4de3718e852c15206b1992d62ee088c3293c32d5
Mozamel Anwary <mozamel.anwary1@gmail.com>
2020-03-22 01:35:09 +1100

    Remove temp token

--- server/src/app.ts
+++ server/src/app.ts
@@ -26,8 +26,7 @@ app.all('/*', (req, res, next) => {
 });
 
 app.get('/articles/', async (req, res) => {
-  // const user: ApiUser = await verifyUser(req.headers.authorization);
-  const user: ApiUser = await verifyUser("eyJhbGciOiJSUzI1NiIsImtpZCI6IjFmODhiODE0MjljYzQ1MWEzMzVjMmY1Y2RiM2RmYjM0ZWIzYmJjN2YiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vc3ltcHQtMTI1OWUiLCJhdWQiOiJzeW1wdC0xMjU5ZSIsImF1dGhfdGltZSI6MTU4NDcwNDg5MCwidXNlcl9pZCI6Im1MYW5iNGJiVWZXZUJIcW1DQndZNDc2WjdrcjEiLCJzdWIiOiJtTGFuYjRiYlVmV2VCSHFtQ0J3WTQ3Nlo3a3IxIiwiaWF0IjoxNTg0NzgzNDg3LCJleHAiOjE1ODQ3ODcwODcsImVtYWlsIjoidGVzdEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsidGVzdEBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.iHgOh4E54SED8rUecQvpeSLt14BI2YhEXEAXWrUWoKiA0dnfcBhTPQ6or5W8n7gDD5k5ck9fpRFnWmhq4iH3gdMc0_t-6OJvlN_2YwqwpjMOOWcfCuVWD_yrEeU9s4brFmvInGvZ5L6Exdp0Wf24-31lwrVYgnerlBDEg7mn4hClg6OpEDfTo_cDE1aaoJuDFIkaISBXNLaKe57glqojmWAob4pMK9ntfbOb_bYjBG1QE_d3EizNcwiWEb4aF7-Sa1b7nCr28mY1FzQ8GtCaJhWxEBhhcF0vYCtmPS7JxP799-KkY1kLouHATa1MTMdF37mV65_DC4TUqGQf09iqqw");
+  const user: ApiUser = await verifyUser(req.headers.authorization);
   const timestamp: string = (new Date().getTime() / 1000).toFixed(0).toString();
 
   if (user.authenticated) {
