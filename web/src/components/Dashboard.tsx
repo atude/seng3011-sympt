fe2c3fa5f2d46d475293985bb623e259a8ab980d
Mozamel Anwary <mozamel.anwary1@gmail.com>
2020-03-23 16:53:26 +1100

    Dev dashboard graph, bug fixes

--- web/src/components/Dashboard.tsx
+++ web/src/components/Dashboard.tsx
@@ -12,7 +12,7 @@ import {
   TableRow, 
   TableCell, 
   TableBody, 
-  LinearProgress, 
+  LinearProgress,
 } from '@material-ui/core';
 import { 
   AccountCircle, 
@@ -20,7 +20,17 @@ import {
   Mail as MailIcon,
   VpnKey as KeyIcon,
   ListAlt,
+  InsertChartOutlined,
 } from '@material-ui/icons';
+import { 
+  AreaChart, 
+  CartesianGrid, 
+  XAxis, 
+  YAxis, 
+  Area, 
+  Tooltip, 
+  ResponsiveContainer,
+} from 'recharts';
 
 import { signOut, refreshToken, fetchLogs } from '../firebase/firebaseFunctions';
 import firebase from '../firebase/firebaseInit';
@@ -34,7 +44,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     margin: "auto",
     maxWidth: "800px",
   },
-  paperParent: {
+  innerPadding: {
     padding: "2rem",
   },
   tableContainer: {
@@ -45,7 +55,7 @@ const useStyles = makeStyles((theme: Theme) => ({
   accountIcon: {
     fontSize: "6rem",
   },
-  dashboardIcon: {
+  headerIcon: {
     padding: "10px",
     marginTop: "7px",
   },
@@ -70,6 +80,7 @@ const Dashboard = (props: any) => {
   const [loading, setLoading] = useState(true);
   const [token, setToken] = useState("-----");
   const [logs, setLogs] = useState<ApiLog[]>([]);
+  const [graphLogs, setGraphLogs] = useState<any>();
   const [showToken, setShowToken] = useState(false);
 
   useEffect(() => {
@@ -80,6 +91,23 @@ const Dashboard = (props: any) => {
 
       const getLogs = await fetchLogs(user.email);
       setLogs(getLogs);
+
+      const initGraphLogs: {[k: string]: any} = {};
+      getLogs.forEach((log: ApiLog) => {
+        const date = moment(Number(log.timestamp) * 1000).format("L");
+        if (initGraphLogs[date]) {
+          initGraphLogs[date].success += log.success ? 1 : 0;
+          initGraphLogs[date].errror += !log.success ? 1 : 0;
+        } else {
+          initGraphLogs[date] = { 
+            date,
+            success: log.success ? 1 : 0, 
+            error: !log.success ? 1 : 0,
+          };
+        }
+      });
+
+      setGraphLogs(Object.values(initGraphLogs).splice(0, 7).reverse());
       setLoading(false);
     };
 
@@ -103,7 +131,7 @@ const Dashboard = (props: any) => {
     <div className={classes.parent}>
       <Grid container direction="row" alignItems="center">
         <Grid item>
-          <DashboardIcon className={classes.dashboardIcon} fontSize="large" color="primary" />
+          <DashboardIcon className={classes.headerIcon} fontSize="large" color="primary" />
         </Grid>
         <Grid item>
           <Typography variant="h5">
@@ -111,9 +139,9 @@ const Dashboard = (props: any) => {
           </Typography>
         </Grid>
       </Grid>
-      <br />
-      <Paper className={classes.paperParent} elevation={4}>
-        <Grid container direction="row" spacing={8} justify="center">
+      <br /><br />
+      <Paper elevation={4}>
+        <Grid className={classes.innerPadding} container direction="row" spacing={8} justify="center">
           <Grid item>
             <Grid item container direction="column" spacing={1} justify="center" alignItems="center">
               <Grid item>
@@ -159,10 +187,60 @@ const Dashboard = (props: any) => {
           </Grid>
         </Grid>
       </Paper>
+      <br /><br />
+      <Grid container direction="row" alignItems="center">
+        <Grid item>
+          <InsertChartOutlined className={classes.headerIcon} fontSize="large" color="primary" />
+        </Grid>
+        <Grid item>
+          <Typography variant="h5">
+            Usage Analysis
+          </Typography>
+        </Grid>
+      </Grid>
+      <Paper 
+        elevation={4} 
+        style={logs.length ? { width: "100%", height: 400, paddingBottom: "50px" } : { padding: "1.6rem" }}
+      >
+        {logs.length ? (
+          <>
+            <ResponsiveContainer>
+              <AreaChart
+                data={graphLogs}
+                margin={{
+                  top: 50, right: 70, left: 20, bottom: 50,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="date" dy={15} />
+                <YAxis />
+                <Tooltip />
+                <Area type="linear" dataKey="success" stroke="#56c149" fill="#56c149" />
+                <Area type="linear" dataKey="error" stroke="#ff4d4d" fill="#ff4d4d" />
+              </AreaChart>
+            </ResponsiveContainer>
+            <Typography 
+              variant="button" 
+              color="secondary" 
+              align="center"
+              display="block"
+            >
+              Usage over the past 7 days
+            </Typography>
+          </>
+        ) : (
+          <Typography 
+            color="secondary" 
+            variant="button" 
+          >
+            Not enough data
+          </Typography>
+        )}
+      </Paper>
       <br />
       <Grid container direction="row" alignItems="center">
         <Grid item>
-          <ListAlt className={classes.dashboardIcon} fontSize="large" color="primary" />
+          <ListAlt className={classes.headerIcon} fontSize="large" color="primary" />
         </Grid>
         <Grid item>
           <Typography variant="h5">
@@ -170,61 +248,69 @@ const Dashboard = (props: any) => {
           </Typography>
         </Grid>
       </Grid>
-      <br />
       <Paper className={classes.tableContainer} elevation={4}>
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Time</TableCell>
-                <TableCell>Query</TableCell>
-                <TableCell>Error</TableCell>
-                <TableCell align="right">Response</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {logs.map((log: ApiLog) => {
-                const date: Date = new Date(Number(log.timestamp) * 1000);
-                const dateStr: string = moment(date).format('LLL');
-                
-                return (
-                  <TableRow key={log.timestamp}>
-                    <TableCell component="th" scope="row">
-                      {dateStr}
-                    </TableCell>
-                    <TableCell style={{ maxWidth: "200px" }}>
-                      {log.query.replace('/articles/', '').split('&').map((section: string, i: number) => (
-                        <div>{i !== 0 && "&"}{section}</div>
-                      ))}
-                    </TableCell>
-                    <TableCell>
-                      {log.error ? (
-                        <>
-                          <div style={{ fontWeight: "bold" }}>
-                            {log.error.errorName}
-                          </div>
-                          <div>
-                            {log.error.errorMessage}
-                          </div>
-                        </>
-                      ) : (
-                        "None"
-                      )}
-                    </TableCell>
-                    <TableCell align="right">
-                      <Typography
-                        variant="button" 
-                        style={{ color: log.success ? "green" : "red" }}
-                      >
-                        {log.success ? "Successful" : `Error (${log?.error?.errorNo})`}
-                      </Typography>
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        {logs.length ? (
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Time</TableCell>
+                  <TableCell>Query</TableCell>
+                  <TableCell>Error</TableCell>
+                  <TableCell align="right">Response</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {logs.map((log: ApiLog) => {
+                  const date: Date = new Date(Number(log.timestamp) * 1000);
+                  const dateStr: string = moment(date).format('LLL');
+                  
+                  return (
+                    <TableRow key={log.timestamp}>
+                      <TableCell component="th" scope="row">
+                        {dateStr}
+                      </TableCell>
+                      <TableCell style={{ maxWidth: "200px" }}>
+                        {log.query.replace('/articles/', '').split('&').map((section: string, i: number) => (
+                          <div>{i !== 0 && "&"}{section}</div>
+                        ))}
+                      </TableCell>
+                      <TableCell>
+                        {log.error ? (
+                          <>
+                            <div style={{ fontWeight: "bold" }}>
+                              {log.error.errorName}
+                            </div>
+                            <div>
+                              {log.error.errorMessage}
+                            </div>
+                          </>
+                        ) : (
+                          "None"
+                        )}
+                      </TableCell>
+                      <TableCell align="right">
+                        <Typography
+                          variant="button" 
+                          style={{ color: log.success ? "green" : "red" }}
+                        >
+                          {log.success ? "Successful" : `Error (${log?.error?.errorNo})`}
+                        </Typography>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        ) : (
+          <Typography 
+            color="secondary" 
+            variant="button" 
+          >
+            Not enough data
+          </Typography>
+        )}
       </Paper>
     </div>
   );
