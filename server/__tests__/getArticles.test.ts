c275f1fe8c30682b7b91214d83b66dea7d132ae3
Matthew Freeman <mattyfreeman101@gmail.com>
2020-03-23 17:59:13 +1100

    moved the getDataObject to its own file for export/import

--- server/__tests__/getArticles.test.ts
+++ server/__tests__/getArticles.test.ts
@@ -1,31 +1,12 @@
 import admin from '../src/firebase/firebaseInit';
 import { getArticles } from '../src/queryController';
 import { isError } from '../src/utils/checkFunctions';
+import { getDateObject } from '../src/utils/getDateObject';
 
 console.log(`Admin init: ${!!admin}`);
 
 jest.setTimeout(30000);
 
-
-const getDateObject = (datestring: string | undefined) => {
-  // datestring must be of the format 'yyyy-mm-dd hh:mm:ss'
-
-  if (!datestring) {
-    return null;
-  }
-
-  const dateTimeRegex = /(\d{4})-(\d{2})-(\d{2})\s(\d{2}):(\d{2}):(\d{2})/g;
-
-  const dateData = datestring.match(dateTimeRegex);
-
-  if (dateData) {
-    const [, year, month, day, hours, mins, secs] = "";
-    const date = new Date(parseInt(year, 10), parseInt(month, 10) - 1, 
-      parseInt(day, 10), parseInt(hours, 10), parseInt(mins, 10), parseInt(secs, 10));
-    return date;
-  }
-  return null;
-};
 
 describe("getArticles", () => {
   test("Check no articles", async () => {
