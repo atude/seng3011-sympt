a1945470bfb6498687059c845d1c7f266b5b3514
Mariya <mar.shmalko@gmail.com>
2020-03-29 15:53:56 +1100

    Added tabs for Articles and Trending

---
+++ app/screens/ArticlesScreen.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+
+export default function ArticlesScreen() {
+  return (
+    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+      <Text>Articles go here!</Text>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+  },
+  contentContainer: {
+    paddingTop: 15,
+  },
+});
--- app/screens/FeedScreen.js
+++ app/screens/FeedScreen.js
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import { StyleSheet, Text, ScrollView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import TopTabFeedNavigator from '../navigation/TopTabFeedNavigator';
 
 export default function FeedScren() {
   return (
     <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
       <Text>This is where articles and newsfeed will go</Text>
+
+      <NavigationContainer independent={true}>
+        <TopTabFeedNavigator />
+      </NavigationContainer>
     </ScrollView>
   );
 }
