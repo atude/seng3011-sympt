9ca6b781743e1541046ca7dcf1a3fa1db53bce11
Mariya <mar.shmalko@gmail.com>
2020-03-31 22:36:07 +1100

    Added map to self reported cases screen

--- app/screens/SelfReportMapScreen.js
+++ app/screens/SelfReportMapScreen.js
@@ -1,16 +1,25 @@
-import React, { useContext } from 'react';
-import { StyleSheet } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import React from 'react';
+import { StyleSheet, View, Dimensions } from 'react-native';
 import Colors from '../constants/Colors';
+import MapView from 'react-native-maps';
 
 import StyledText from '../components/StyledText';
+import StyledCard from '../components/StyledCard';
 
 const SelfReportMapScreen = (props) => {
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <StyledText>Ah yes.. maps</StyledText>
-    </ScrollView>
+    <View contentContainerStyle={styles.container}>
+      <MapView style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+      <MapView />
+    </View>
   );
 };
 
@@ -18,7 +27,12 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: Colors.bg,
     padding: 24,
-  }, 
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
 });
 
 export default SelfReportMapScreen;
