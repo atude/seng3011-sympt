993f3915f398a2baa1d710c9b763b94037a0a1c0
Matthew Freeman <mattyfreeman101@gmail.com>
2020-03-31 16:52:26 +1100

    trending screen logic completed, just need to format and refactor

--- app/screens/TrendingScreen.js
+++ app/screens/TrendingScreen.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState, useEffect, useContext } from 'react';
+import { StyleSheet, ActivityIndicator } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import Colors from '../constants/Colors';
 import StyledCard from '../components/StyledCard';
 import StyledText from '../components/StyledText';
+import getTrendingArticles from '../functions/trendingFunctions';
+import { DiseaseContext } from '../context/context';
 
 export default function TrendingScreen() {
+  const diseaseContext = useContext(DiseaseContext);
+  const [articles, setArticles] = useState(null);
+
+  useEffect(() => {
+    if (!articles) {
+      const fetchTrendingArticles = async () => {
+        const articlesResponse = await getTrendingArticles(diseaseContext.diseaseName);
+        setArticles(articlesResponse);
+      };
+      fetchTrendingArticles();
+    }
+  }, [articles]);
+
   return (
     <ScrollView style={styles.containerParent} contentContainerStyle={styles.container}>
-      <StyledCard>
-        <StyledText>
-          hello this is some styled text
-        </StyledText>
-      </StyledCard>
+      {articles ? formatArticles(articles) : <ActivityIndicator size='large' color={Colors.primary}/>}
     </ScrollView>
   );
 }
@@ -26,3 +37,18 @@ const styles = StyleSheet.create({
     height: "100%",
   }
 });
+
+const formatArticles = (articles) => {
+  if(!articles) {
+    return (<StyledCard><StyledText>There are no news articles or tweets</StyledText></StyledCard>);
+  }
+  const formattedArticles = articles.map((article, i) => {
+    return (
+    <StyledCard key={i}>
+      <StyledText>
+        {article.title}
+      </StyledText>
+    </StyledCard>);
+  });
+  return formattedArticles;
+};
