df25415794ab69e33872dd6af86519265fd85ffc
Matthew Freeman <mattyfreeman101@gmail.com>
2020-03-31 16:53:28 +1100

    template news card component ready for implementation

---
+++ app/components/NewsCard.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import StyledCard from './StyledCard';
+import StyledText from './StyledText';
+
+// implement later
+const NewsCard = () => {
+    return null;
+};
