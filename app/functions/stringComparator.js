2407549703f002bb705cc30304c4b4437aa64cc2
Matthew Freeman <mattyfreeman101@gmail.com>
2020-04-02 01:01:32 +1100

    a string comparator for the diseases list specifically

---
+++ app/functions/stringComparator.js
@@ -0,0 +1,11 @@
+function StringComparator(a, b) {
+  let comparison = 0;
+  if (a.name > b.name) {
+    comparison = 1;
+  } else if (a.name < b.name) {
+    comparison = -1;
+  }
+  return comparison;
+}
+
+export default StringComparator;
