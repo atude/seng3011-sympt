791fdea530cf1e1a9e5417e1aa26387e7fcf16e0
Mozamel Anwary <mozamel.anwary1@gmail.com>
2020-04-03 00:03:32 +1100

    Disease scraper implementation, fix hot reload on server

--- server/src/firebase/collectionReferences.ts
+++ server/src/firebase/collectionReferences.ts
@@ -2,3 +2,4 @@ import * as admin from 'firebase-admin';
 
 export const articlesRef = admin.firestore().collection("articles");
 export const apiUsersRef = admin.firestore().collection("apiUsers");
+export const casesRef = admin.firestore().collection("cases");
