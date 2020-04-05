f4d6f281ad05ba4e09b8d80ff8f9de00d34ff53e
Mariya <mar.shmalko@gmail.com>
2020-04-05 21:48:07 +1000

    Fixed for deployment

--- app/functions/selfReportedCasesFunctions.js
+++ app/functions/selfReportedCasesFunctions.js
@@ -1,24 +1,12 @@
-import firebase from '../firebase/firebaseInit';
 import { getFetchMeta } from "../utils/fetchTools";
-
-
 
 export const setSymptoms = async (userContext, selectedMap) => {
   const selectedSymptoms = [...selectedMap.keys()].toString();
   const selectedDetails = [];
 
-  const data = {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json'
-    }
-  };
-
   try {
-    // let response = await fetch(`https://sympt-server.herokuapp.com/_userDetails/?symptoms=${selectedSymptoms}&details=${selectedDetails}`, 
-    //   getFetchMeta(userContext.user.uid));
     let response = await fetch(`https://sympt-server.herokuapp.com/_userDetails/?symptoms=${selectedSymptoms}&details=${selectedDetails}`, 
-      data);
+      getFetchMeta(userContext.user.uid));
     response = await response.json();
     return response;
   } catch (error) {
@@ -27,8 +15,3 @@ export const setSymptoms = async (userContext, selectedMap) => {
 
   return null;
 };
-
-// export const getSelfReportedCases = () => { // Should return array of coordinates of sick people... 
-
-
-// }
