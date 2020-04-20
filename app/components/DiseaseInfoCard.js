6ed6f20c55f855e4027ea33a07db4a38e94376fd
Mariya <mar.shmalko@gmail.com>
2020-04-20 15:51:49 +1000

    Added icon

--- app/components/DiseaseInfoCard.js
+++ app/components/DiseaseInfoCard.js
@@ -1,14 +1,21 @@
 import React, { useContext } from 'react';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View, Image, TouchableOpacity } from 'react-native';
 import StyledText from './StyledText';
 import { DiseaseContext } from '../context/context';
 import { getDiseaseImage } from '../utils/mapDiseaseImages';
 import Colors from '../constants/Colors';
 import StyledCard from './StyledCard';
+import { Ionicons } from '@expo/vector-icons';
+import TabBarIcon from '../components/TabBarIcon';
 
 const DiseaseInfoCard = (props) => {
   const diseaseContext = useContext(DiseaseContext);
   const {disease} = props;
+
+  const handleClick = () => {
+    console.log("Closing");
+  };
+
 
   return (
     <StyledCard style={[styles.container, {
@@ -17,6 +24,11 @@ const DiseaseInfoCard = (props) => {
         'transparent' 
     }]}>
       <View style={styles.contentContainer}>
+        <TouchableOpacity onPress={() => handleClick()}>
+          <TabBarIcon 
+            name="arrow-down-drop-circle-outline" 
+          />
+        </TouchableOpacity>
         <Image source={getDiseaseImage(disease.name)} style={styles.diseaseImage}/>
         <View style={styles.headerContainer}>
           <StyledText style={styles.heading}>
@@ -81,6 +93,9 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
   },
+  collapseIcon: {
+
+  }
 });
 
 export default DiseaseInfoCard;
